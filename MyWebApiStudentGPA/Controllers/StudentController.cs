aefcd7f7036946adf88f5b9f93e7af647c25bee1
MAAN-G <zaidmaan111@gmail.com>
2023-12-03 18:27:05 +0500

    Add project files.

---
+++ MyWebApiStudentGPA/Controllers/StudentController.cs
@@ -0,0 +1,82 @@
+﻿using DL.DbModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWebApiStudentGPA.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+        private readonly StudentDbContext dbContext;
+        public StudentController(StudentDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET: https://localhost:portnumber/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            var students = dbContext.studentDbDto.ToList();
+            return Ok(students);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create([FromBody] StudentDbDto studentDbDto)
+        {
+
+
+            dbContext.studentDbDto.Add(studentDbDto);
+            dbContext.SaveChanges();
+
+            return Ok();
+
+        }
+
+        [HttpPut("{studentId}")]
+        public IActionResult EditStudent([FromRoute] int studentId, [FromBody] StudentDbDto updatedStudent)
+        {
+
+            if (studentId != updatedStudent.Id)
+            {
+                return BadRequest();
+            }
+
+            var student = dbContext.studentDbDto.Find(studentId);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            student.Name = updatedStudent.Name;
+            student.RollNumber = updatedStudent.RollNumber;
+            student.PhoneNumber = updatedStudent.PhoneNumber;
+
+            dbContext.SaveChanges();
+            return Ok("Updated Successfully");
+        }
+
+
+
+        [HttpDelete("{studentId}")]
+        public IActionResult DeleteStudent([FromRoute] int studentId)
+        {
+            var student = dbContext.studentDbDto.Find(studentId);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.studentDbDto.Remove(student);
+
+            dbContext.SaveChanges();
+
+            return Ok("Student Deleted Successfully");
+        }
+    }
+
+
+}
---
+++ MyWebApiStudentGPA/Controllers/SubjectController.cs
@@ -0,0 +1,83 @@
+﻿using DL.DbModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyWebApiStudentGPA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        private readonly StudentDbContext dbContext;
+        public SubjectController(StudentDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET: api/<SubjectController>
+        [HttpGet]
+        public IActionResult  GetSubjects()
+        {
+            var subjects = dbContext.subjectDbDto.ToList();
+            return Ok(subjects);
+        }
+
+       
+        [HttpGet("{subjectId}")]
+        public IActionResult Get(int subjectId)
+        {
+            var subject = dbContext.subjectDbDto.Find(subjectId);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            return Ok(subject);
+        }
+
+        [HttpPost]
+        public IActionResult AddSubject([FromBody] SubjectDbDto subjectDbDto)
+        {
+            dbContext.subjectDbDto.Add(subjectDbDto);
+            dbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPut("{subjectId}")]
+        public IActionResult EditSubject([FromRoute]int subjectId, [FromBody] SubjectDbDto updatedSubject)
+        {
+            if (subjectId != updatedSubject.id)
+            {
+                return BadRequest();
+            }
+
+            var subject = dbContext.subjectDbDto.Find(subjectId);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            subject.Name = updatedSubject.Name;
+            
+            dbContext.SaveChanges();
+            return Ok("Updated Successfully");
+        }
+
+        [HttpDelete("{subjectId}")]
+        public IActionResult DeleteStudent([FromRoute] int subjectId)
+        {
+            var subject = dbContext.subjectDbDto.Find(subjectId);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.subjectDbDto.Remove(subject);
+
+            dbContext.SaveChanges();
+
+            return Ok("Student Deleted Successfully");
+        }
+    }
+}
